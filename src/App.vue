c61e14bcf686a9d1332837999a35511d76833163
Maxime Dréau <maxime@dreau.fr>
2021-05-09 16:01:12 +0200

    Bootstrap vite project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <v-app>
-    <router-view />
-  </v-app>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
-<style lang="sass">
-html,
-body
-  height: 100vh
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/config.js
+++
@@ -1,4 +0,0 @@
-export const config = {
-  clientId: '4d4450d11db8449097d50daa39321192',
-  clientSecret: '86e4e71ae6834e6bb2ce17b32dd47ee9'
-}
