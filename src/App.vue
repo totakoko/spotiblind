afc8008b39a2b46218a4fda031c61ba077b0d34c
Maxime Dréau <maxime@dreau.fr>
2023-12-23 15:33:29 +0100

    Fix the height on mobile

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,8 @@
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
   color: #2c3e50
-  height: 100vh
+  height: 100vh // fallback
+  height: 100dvh
   display: flex
   flex-direction: column
   background-color: #f5f5f5
